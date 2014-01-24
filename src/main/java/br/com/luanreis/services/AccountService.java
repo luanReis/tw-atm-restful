1e7e6db73f0bedf7013ea46e97cda85ede00c95c
luanReis <luan.rpsantos@gmail.com>
2014-01-24 15:58:31 -0300

    Added deposite feature by rest call

--- src/main/java/br/com/luanreis/services/AccountService.java
+++ src/main/java/br/com/luanreis/services/AccountService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -13,14 +15,16 @@ import javax.ws.rs.core.Response;
 
 import org.hibernate.Session;
 
+import br.com.luanreis.exceptions.AccountManagementException;
 import br.com.luanreis.models.Account;
 import br.com.luanreis.util.HibernateUtil;
 
 @Path("accounts")
 @Produces(MediaType.APPLICATION_JSON)
 public class AccountService {
+
 	@GET
-	public List<Account> get() {
+	public List<Account> getAll() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
 		List<Account> result = session.createQuery("from Account").list();
@@ -29,17 +33,24 @@ public class AccountService {
 		return result;
 	}
 
+	private Account get(long accountNumber) {
+		Account account = null;
+		for (Account storedAccount : getAll()) {
+			if (storedAccount.getAccountNumber() == accountNumber) {
+				account = storedAccount;
+				break;
+			}
+		}
+		return account;
+	}
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response add(Account account) {
 		if (account.getBalance() != 0) {
 			throw new WebApplicationException(422);
-		}
-
-		for (Account storedAccount : get()) {
-			if (account.getAccountNumber() == storedAccount.getAccountNumber()) {
-				throw new WebApplicationException(422);
-			}
+		} else if (get(account.getAccountNumber()) != null) {
+			throw new WebApplicationException(409);
 		}
 
 		Session session = HibernateUtil.getSessionFactory().openSession();
@@ -49,6 +60,28 @@ public class AccountService {
 		session.close();
 
 		return Response.status(Response.Status.CREATED).entity(account).build();
+	}
+
+	@Path("{accountNumber}/{amount}")
+	@PUT
+	public void deposit(@PathParam("accountNumber") long accountNumber,
+			@PathParam("amount") double amount) {
+
+		Account account = get(accountNumber);
+		if (account != null) {
+			try {
+				account.deposit(amount);
+				Session session = HibernateUtil.getSessionFactory()
+						.openSession();
+
+				session.beginTransaction();
+				session.update(account);
+				session.getTransaction().commit();
+				session.close();
+			} catch (AccountManagementException e) {
+				throw new WebApplicationException(400);
+			}
+		}
 	}
 
 }
